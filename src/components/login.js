0eb636a4c4c5550acb9a1a67ad845e2050f6e993
phucpham24 <phucphamhcmut@gmail.com>
2025-03-25 19:53:15 +0100

    change methode

--- src/components/login.js
+++ src/components/login.js
@@ -43,7 +43,7 @@ const Login = () => {
         toast.success("Login successful!");
         navigate("/")
     } else {
-        if(res && +res.status === 400){
+        if(res && +res.status >= 400){
             toast.error("Username/Passwords incorrect");
         }
         
@@ -55,7 +55,7 @@ const Login = () => {
 return (
     <div className="login-container">
         <div className="login-box">
-            <h2>Log in tester1@gmail.com</h2>
+            <h2>Log in testerpa3@gmail.com</h2>
             <form onSubmit={(e) => e.preventDefault()}>
                 <input
                     type="email"
